f2a2d85806e9ce90fa4b6a622e86fec03038db23
Mohit Arora <mohit.arora@sydney.edu.au>
2021-04-10 12:18:30 +1000

    new features added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // reference to important DOM custom elements
 var timeDisplayEl = $('#currentDay');
 
-// adding the current day, date and time in the jubotron
+// adding the current day, date and time
 function displayTime (){
     var liveDay = moment().format('dddd');
     var liveDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
