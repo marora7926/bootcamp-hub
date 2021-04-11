6c7e843fc8d4f4e8b269583531d6168b3b54410d
Mohit Arora <mohit.arora@sydney.edu.au>
2021-04-11 16:20:27 +1000

    updated css and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,7 @@ setInterval(displayTime, 1000);
 // add click event --- MOHIT TO FIX THIS
 // <button class="btn btn-block btn-info">Learn more</button>
 // <a href="https://www.w3schools.com/html/default.asp" target="_blank"></a>
+// CSS : https://www.w3schools.com/css/default.asp
+// JS: https://www.w3schools.com/js/default.asp
+// jQuery: https://www.w3schools.com/jquery/default.asp
+// Bootstrap: https://www.w3schools.com/bootstrap4/default.asp
