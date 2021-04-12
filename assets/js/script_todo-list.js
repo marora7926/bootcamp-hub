ecfdca9ee8a3af46e609aed4d71339fc00e51c05
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-04-12 18:30:56 +1000

    To Do page initialised

---
+++ assets/js/script_todo-list.js
@@ -0,0 +1,54 @@
+var formEl = $('#todo-form');
+var nameInputEl = $('#todo-name');
+var dateInputEl = $('#datepicker');
+var todoListEl = $('#todo-list');
+
+var printTodos = function (name, date) {
+  var listEl = $('<li>');
+  var listDetail = name.concat(' on ', date);
+  listEl.addClass('list-group-item').text(listDetail);
+  listEl.appendTo(todoListEl);
+};
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var nameInput = nameInputEl.val();
+  var dateInput = dateInputEl.val();
+
+  if (!nameInput || !dateInput) {
+    console.log('You need to fill out the form!');
+    return;
+  }
+
+  printSkills(nameInput, dateInput);
+
+  // resets form
+  nameInputEl.val('');
+  dateInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit);
+
+// Autocomplete widget
+$(function () {
+  var todoNames = [
+    'Revise',
+    'Homework',
+    'Group Meeting',
+    'Group Assignment',
+    'Watch Video',
+    'Office Hours',
+  ];
+  $('#todo-name').autocomplete({
+    source: todoNames,
+  });
+});
+
+// Datepicker widget
+$(function () {
+  $('#datepicker').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+});
