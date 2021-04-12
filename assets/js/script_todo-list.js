2de3e2bc34899b748fdc5b7d6bb80abe7c3abee6
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-04-12 20:17:32 +1000

    Added remove button to list

--- assets/js/script_todo-list.js
+++ assets/js/script_todo-list.js
@@ -7,8 +7,24 @@ var printTodos = function (name, date) {
   var listEl = $('<li>');
   var listDetail = name.concat(' on ', date);
   listEl.addClass('list-group-item').text(listDetail);
+    // add delete button to remove shopping list item
+    listEl.append(
+        '<button class="btn btn-danger btn-small delete-item-btn float-right">Remove</button>'
+      );
   listEl.appendTo(todoListEl);
 };
+
+function handleRemoveItem(event) {
+    // convert button we pressed (`event.target`) to a jQuery DOM object
+    var btnClicked = $(event.target);
+  
+    // get the parent `<li>` element from the button we pressed and remove it
+    btnClicked.parent('li').remove();
+  }
+  
+  // use event delegation on the `shoppingListEl` to listen for click on any element with a class of `delete-item-btn`
+  todoListEl.on('click', '.delete-item-btn', handleRemoveItem);
+  //formEl.on('submit', handleFormSubmit);
 
 var handleFormSubmit = function (event) {
   event.preventDefault();
@@ -22,6 +38,9 @@ var handleFormSubmit = function (event) {
   }
 
   printTodos(nameInput, dateInput);
+
+
+  //currTodos.name[i], currTodos.date[i]
 
   // resets form
   nameInputEl.val('');
@@ -52,3 +71,11 @@ $(function () {
     changeYear: true,
   });
 });
+
+/*
+var currTodos = localStorage.getItem("localList");
+for (var i=0; i< currTodos.length; i++){
+    printTodos(currTodos.name[i], currTodos.date[i]);
+}
+*/
+//printTodos(nameInput, dateInput);
